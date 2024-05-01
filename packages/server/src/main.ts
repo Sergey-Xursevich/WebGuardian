5e7d9dd79571a900ef9c852493aeadfa842efdba
Sergey Khursevich <sergey.khursevich@sast-mfa.com>
2024-05-01 14:22:21 +0200

    feat: add basic tools for app

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder().setTitle('Web Guardian').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(4000);
 }
 bootstrap();
